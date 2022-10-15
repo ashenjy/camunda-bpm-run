349e73b8f5129a7d458770f18e92fca5b3900a8c
ashen <jayasinghe.ashen@gmail.com>
2022-10-15 16:28:51 +0530

    initial spring boot

---
+++ src/main/java/com/cn/camunda/CamundaApplication.java
@@ -0,0 +1,32 @@
+package com.cn.camunda;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+@EnableProcessApplication("bpm-run")
+public class CamundaApplication {
+
+//  @Autowired
+//  RuntimeService runtimeService;
+
+  public static void main(String... args) {
+    SpringApplication.run(CamundaApplication.class, args);
+  }
+
+////   This is only used for testing purposes
+//  @Bean
+//  public CommandLineRunner createDemoProcessInstance(){
+//
+//    return (String[] args) -> runtimeService.startProcessInstanceByKey("bpm-run");
+//
+//  }
+
+}
